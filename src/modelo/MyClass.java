bce15a0743d837f19282a87ff8f62437481b4e9e
AlumnoMañana <AlumnoMaana@T20211>
2018-12-11 08:19:45 +0000

    Punto 1 completo

--- src/modelo/MyClass.java
+++ src/modelo/MyClass.java
@@ -2,5 +2,83 @@ package modelo;
 
 public class MyClass {
 	// Objetivos 1 y 2
+		float prop1;
+		int prop2;
+		String prop3;
+		char prop4;
+		boolean prop5;
+		
+		
+		public MyClass() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
 
+
+		public MyClass(float prop1, int prop2, String prop3, char prop4, boolean prop5) {
+			super();
+			this.prop1 = prop1;
+			this.prop2 = prop2;
+			this.prop3 = prop3;
+			this.prop4 = prop4;
+			this.prop5 = prop5;
+		}
+
+
+		public float getProp1() {
+			return prop1;
+		}
+
+
+		public void setProp1(float prop1) {
+			this.prop1 = prop1;
+		}
+
+
+		public int getProp2() {
+			return prop2;
+		}
+
+
+		public void setProp2(int prop2) {
+			this.prop2 = prop2;
+		}
+
+
+		public String getProp3() {
+			return prop3;
+		}
+
+
+		public void setProp3(String prop3) {
+			this.prop3 = prop3;
+		}
+
+
+		public char getProp4() {
+			return prop4;
+		}
+
+
+		public void setProp4(char prop4) {
+			this.prop4 = prop4;
+		}
+
+
+		public boolean isProp5() {
+			return prop5;
+		}
+
+
+		public void setProp5(boolean prop5) {
+			this.prop5 = prop5;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
 	}
