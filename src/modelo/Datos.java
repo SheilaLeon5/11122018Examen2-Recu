ae40bc59900450fe3d8391c1d4fac8b21798437c
AlumnoMañana <AlumnoMaana@T20211>
2018-12-11 09:44:47 +0000

    .

--- src/modelo/Datos.java
+++ src/modelo/Datos.java
@@ -10,4 +10,33 @@ public class Datos {
 private String[] equipos = { "Real Madrid", "Barcelona", "Sevilla" };
 private String[] jornadas = { "fecha1", "fecha2", "fecha3", "fecha4", "fecha5" };
 
+public int[][] getGolesJornadasEquipos() {
+	return golesJornadasEquipos;
+}
+
+
+
+
+public void setGolesJornadasEquipos(int[][] golesJornadasEquipos) {
+	this.golesJornadasEquipos = golesJornadasEquipos;
+}
+public String[] getEquipos() {
+	return equipos;
+}
+public void setEquipos(String[] equipos) {
+	this.equipos = equipos;
+}
+public String[] getJornadas() {
+	return jornadas;
+}
+public void setJornadas(String[] jornadas) {
+	this.jornadas = jornadas;
+}
+
+
+
+
+
+
+
 }
--- src/recu/Objetivos.java
+++ src/recu/Objetivos.java
@@ -1,10 +1,70 @@
 package recu;
 
+import java.util.Arrays;
+
+import modelo.Datos;
 import modelo.MyClass;
 
 public class Objetivos {
 
 	// Desarrolllar los métodos del examen AQUI (ver Objetivos)
+	
+	//apartado 3.
+	public void listaGoles() {
+		Datos dato= new Datos();
+		 int[][] golesJornadasEquipos = dato.getGolesJornadasEquipos();
+		 for (int i = 0; i < golesJornadasEquipos.length; i++) {
+			 int acumulado = 0;
+
+			for (int j = 0; j < golesJornadasEquipos[0].length; j++) {
+				acumulado += golesJornadasEquipos[i][j];
+				
+			}
+			//System.out.println("Fecha " + i + ":" + acumulado );
+		}
+		 
+		
+	}
+	
+	//apartado4.
+	
+	//PENDIENTEEEEEEEEEEEEEEEEE
+	 public int dividirDosNumeros (int a, int b) {
+		 int resultado = a-b;
+		 while(resultado != 0) {
+			 resultado = a-b; 
+		 }
+		return b;
+	 }
+	
+	//apartado5.
+	 public void listaGolesJornada() {
+		 Datos dato= new Datos();
+		 int[][] golesJornadasEquipos = dato.getGolesJornadasEquipos();
+		 String[] equipos = dato.getEquipos();
+		 for (int j = 0; j < equipos.length; j++) {
+			 int acumulado = 0;
+			for (int i = 0; i < equipos[0].length(); i++) {
+				acumulado += golesJornadasEquipos[i][j];
+				System.out.println(equipos[j] + ":" + acumulado);
+			}
+		}
+	 }
+	 
+	 
+	 //apartado 6.
+	 
+	 public MyClass[] creaObjetosMyClass(int n) {
+		  MyClass[] array= new MyClass[n];
+		  int i=0;
+		 while(i < n) {
+			array[i] = new MyClass();
+		 }
+		return array;
+	 }
+	 
+	 
+	 
 	public static void main(String[] args) {
 		// Hacer las llamadas a los métodos AQUI.
 		// (no olvidar recoger los valores devueltos
@@ -16,5 +76,19 @@ public class Objetivos {
 		
 		//apartado 2.2
 		MyClass[] array = {objeto1,objeto2,objeto3,null,null};
+		
+		//apartado 3.
+		Objetivos objetivo= new Objetivos();
+		objetivo.listaGoles();
+		
+		//apartado 4.
+		 int resultado4 = objetivo.dividirDosNumeros(27,9);
+		 
+		 //apartado 5.
+		 objetivo.listaGolesJornada();
+		 
+		 //apartos 6.
+		 
+		MyClass[] resultado6 = objetivo.creaObjetosMyClass(2);
 	}
 }
