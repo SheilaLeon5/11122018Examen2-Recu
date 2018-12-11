c80b542f319d31eee7bdd70630323c0c4d6dbc73
AlumnoMañana <AlumnoMaana@T20211>
2018-12-11 08:22:47 +0000

    Punto 2 completo

--- src/recu/Objetivos.java
+++ src/recu/Objetivos.java
@@ -1,10 +1,20 @@
 package recu;
 
+import modelo.MyClass;
+
 public class Objetivos {
 
 	// Desarrolllar los métodos del examen AQUI (ver Objetivos)
 	public static void main(String[] args) {
 		// Hacer las llamadas a los métodos AQUI.
 		// (no olvidar recoger los valores devueltos
+		
+		// apartado 2.1
+		MyClass objeto1 = new MyClass();
+		MyClass objeto2 = new MyClass();
+		MyClass objeto3 = new MyClass();
+		
+		//apartado 2.2
+		MyClass[] array = {objeto1,objeto2,objeto3,null,null};
 	}
 }
